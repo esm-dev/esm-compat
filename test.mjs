74b381f19a62c0fcb61ea4081402f38f0648c81e
Je Xia <i@jex.me>
2024-10-03 12:29:52 +0800

    Update test data

--- test.mjs
+++ test.mjs
@@ -7,12 +7,16 @@ const testData = {
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36": "es2024",
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15": "es2021",
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.1": "es2023",
+  "HeadlessChrome/90": "es2021",
   "HeadlessChrome/109": "es2023",
+  "ES/2020": "es2020",
   "ES/2022": "es2022",
   "Deno/1.33.1": "deno",
   "Deno/1.33.2": "denonext",
-  "Node.js/20.0.0": "node",
   "undici": "node",
+  "Node.js/20.0.0": "node",
+  "Node/20.0.0": "node",
+  "Bun/1.0.0": "node",
 };
 
 for (const [ua, expected] of Object.entries(testData)) {
